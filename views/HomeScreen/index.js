4f38595211df36379078d2a56c8338d749782fe0
Eduardo Vencovsky <eduvencovsky@gmail.com>
2020-04-22 21:32:06 -0300

    Added Names

--- views/HomeScreen/index.js
+++ views/HomeScreen/index.js
@@ -8,12 +8,12 @@ const HomeScreen = () => {
       <Title>Trabalho Engenharia de Software 2</Title>
       <Divider />
       <Subheading>Participantes</Subheading>
-      <List.Item title="Eduardo Vencovsky 201710281" />
-      <List.Item title="Erick Domingos Modenez 201710299" />
-      <List.Item title="Igor Prata 201710277" />
-      <List.Item title="Leonardo Vencovsky 201710276" />
-      <List.Item title="Vitor Facioli 201710292" />
-      <List.Item title="Yan Trevizam 201710283" />
+      <List.Item title="Eduardo Vencovsky" description="201710281" />
+      <List.Item title="Erick Domingos Modenez" description="201710299" />
+      <List.Item title="Igor Prata" description="201710277" />
+      <List.Item title="Leonardo Vencovsky" description="201710276" />
+      <List.Item title="Vitor Facioli" description="201710292" />
+      <List.Item title="Yan Trevizam" description="201710283" />
     </View>
   )
 }
